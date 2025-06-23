dc4b88f7d9ccfbfb7f4f124ec360d0686e62f0d3
Taaate <an981022@naver.com>
2025-06-23 17:24:25 +0900

    Initial Commit

---
+++ src/main/java/com/otatime_server/OtatimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.otatime_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtatimeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtatimeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otatime_server/OtatimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otatime_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtatimeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

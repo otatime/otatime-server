9a4428e522e531b283b536c58339dda577d90e07
Taaate <an981022@naver.com>
2025-09-15 20:48:03 +0900

    fix: 레디스 비밀번호 인증 추가

--- src/main/java/com/otatime_server/config/RedisConfig.java
+++ src/main/java/com/otatime_server/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -16,12 +17,22 @@ public class RedisConfig {
 
     @Value("${redis.host}")
     private String host;
+
     @Value("${redis.port}")
     private int port;
 
+    @Value("${redis.password:}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactoryCustom() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        if (password != null && !password.isEmpty()) {
+            config.setPassword(password);
+        }
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
@@ -42,4 +53,3 @@ public class RedisConfig {
     }
 
 }
-
